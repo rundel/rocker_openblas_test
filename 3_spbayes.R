09ea5ef30ca43fecc288dc436dea85ca3b8142ff
Colin Rundel <rundel@gmail.com>
2019-02-15 20:35:24 -0500

    update

--- 3_spbayes.R
+++ 3_spbayes.R
@@ -2,17 +2,25 @@ RhpcBLASctl::blas_set_num_threads(4)
 
 suppressMessages( library(spBayes) )
 
-readRDS("data.rds")
+site_data = readRDS("/data.rds")
 
-system.time({
-  lm = spLM(
-    pm25 ~ 1, data = site_data, coords = cbind(x=site_data$day, y=0),
-    starting = list(sigma.sq = 1,   tau.sq = 1,   phi = sqrt(3)/30),
-    tuning   = list(sigma.sq = 0.1, tau.sq = 0.1, phi = 0.1),
-    priors   = list(beta.Norm = list(0,1000), phi.Unif = c(sqrt(3)/30-1e-6, sqrt(3)/30+1e-6),
-                    sigma.sq.IG = c(2, 2), tau.sq.IG = c(2, 0.1)),
-    cov.model = "gaussian",
-    amcmc = list(n.batch = 1000, batch.length = 50, accept.rate = 0.43),
-    verbose = FALSE
-  )
-})
+
+parallel::mclapply(
+  1,
+  function(x) {
+    system.time({
+      lm = spLM(
+        pm25 ~ 1, data = site_data, coords = cbind(x=site_data$day, y=0),
+        starting = list(sigma.sq = 1,   tau.sq = 1,   phi = sqrt(3)/30),
+        tuning   = list(sigma.sq = 0.1, tau.sq = 0.1, phi = 0.1),
+        priors   = list(beta.Norm = list(0,1000), phi.Unif = c(sqrt(3)/30-1e-6, sqrt(3)/30+1e-6),
+                        sigma.sq.IG = c(2, 2), tau.sq.IG = c(2, 0.1)),
+        cov.model = "gaussian",
+        amcmc = list(n.batch = 1000, batch.length = 50, accept.rate = 0.43),
+        verbose = FALSE
+      )
+    })
+  },
+  mc.cores = 1
+)
+
--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 all: test-0.2.19 test-0.3.5
 
 test-0.2.19: openblas-0.2.19/Dockerfile
-	cd openblas-0.2.19; docker build --tag rundel/rocker_openblas_test:0.2.19 .
+	docker build --rm --tag rundel/rocker_openblas_test:0.2.19 -f openblas-0.2.19/Dockerfile .
+	docker push rundel/rocker_openblas_test:0.2.19
 	
 test-0.3.5: openblas-0.3.5/Dockerfile
-	cd openblas-0.3.5; docker build --tag rundel/rocker_openblas_test:0.3.5 .
+	docker build --rm --tag rundel/rocker_openblas_test:0.3.5 -f openblas-0.3.5/Dockerfile .
+	docker push rundel/rocker_openblas_test:0.3.5
 
 .PHONY: test-0.2.19 test-0.3.5
